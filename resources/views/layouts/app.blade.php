5b6b9dad1f76bcf3c0ac3f04c09dfc5160530ca9
Cindy Leschaud <cindy.leschaud@gmail.com>
2020-01-06 14:04:10 +0100

    login frontend

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -43,11 +43,6 @@
                             <li class="nav-item">
                                 <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
                             </li>
-                            @if (Route::has('register'))
-                                <li class="nav-item">
-                                    <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
-                                </li>
-                            @endif
                         @else
                             <li class="nav-item dropdown">
                                 <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
@@ -72,7 +67,7 @@
             </div>
         </nav>
 
-        <main class="py-4">
+        <main class="py-4 mt-4">
             @yield('content')
         </main>
     </div>
