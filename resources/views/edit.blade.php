f1d2d8d1da09fb1d09401d1bfce5902748973176
Cindy Leschaud <cindy.leschaud@gmail.com>
2020-10-19 09:21:08 +0200

    max-width edit markdown

--- resources/views/edit.blade.php
+++ resources/views/edit.blade.php
@@ -1,6 +1,6 @@
 @extends('layouts.app')
 @section('content')
-    <div class="container">
+    <div class="container" style="max-width:80% !important;">
         <div class="row justify-content-center">
             <div class="col-md-12">
                 <div class="card">
